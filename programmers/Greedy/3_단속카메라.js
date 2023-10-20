21e682f1d0bf6dd6c03851c56b75cf92a3ea7949
tamagoyakii <rlawlgus4063@naver.com>
2023-10-20 15:05:03 +0900

    [feat] 단속카메라

--- programmers/Greedy/3_단속카메라.js
+++ programmers/Greedy/3_단속카메라.js
@@ -1,4 +1,13 @@
 function solution(routes) {
-  var answer = 0;
-  return answer;
+  let cameras = 0;
+
+  routes
+    .sort(([, a], [, b]) => a - b)
+    .reduce((acc, cur) => {
+      if (cur[0] <= acc) return acc;
+      cameras++;
+      return cur[1];
+    }, -30001);
+
+  return cameras;
 }
