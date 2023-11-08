7bb7b529686a3f8a3fc4a2d6f51390ecb377beb5
tamagoyakii <rlawlgus4063@naver.com>
2023-11-08 22:50:13 +0900

    [feat] 가장 먼 노드

---
+++ programmers/BFS/3_가장 먼 노드.js
@@ -0,0 +1,25 @@
+function solution(n, edge) {
+  const node = new Map();
+
+  edge.forEach(([a, b]) => {
+    node.has(a) ? node.get(a).push(b) : node.set(a, [b]);
+    node.has(b) ? node.get(b).push(a) : node.set(b, [a]);
+  });
+
+  let ans = 0;
+  let queue = [1];
+
+  while (queue.length) {
+    const newQueue = [];
+
+    queue.forEach((currentNode) => {
+      if (!node.has(currentNode)) return;
+      node.get(currentNode).forEach((nextNode) => newQueue.push(nextNode));
+      node.delete(currentNode);
+    });
+
+    queue = newQueue;
+    if (node.size) ans = node.size;
+  }
+  return ans;
+}
