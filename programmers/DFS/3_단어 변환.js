4ea156e61502e71f8fa32c6874ff8dcef2e462d6
tamagoyakii <rlawlgus4063@naver.com>
2023-11-16 18:01:09 +0900

    [feat] 단어 변환

--- programmers/DFS/3_단어 변환.js
+++ programmers/DFS/3_단어 변환.js
@@ -1,7 +1,7 @@
 function solution(begin, target, words) {
   if (words.includes(target) === false) return 0;
 
-  const dfs = (from, trace) => {
+  const changeWord = (from, trace) => {
     const to = words.filter((t) => {
       let diff = 0;
       for (let i = 0; i < t.length; i++) {
@@ -9,11 +9,16 @@ function solution(begin, target, words) {
       }
       return diff === 1 && trace.indexOf(t) === -1;
     });
+
     if (to.length === 0) return 0;
     if (to.includes(target)) return 1;
+
     return (
-      Math.min(...to.map((t) => dfs(t, trace.concat(t))).filter((t) => t)) + 1
+      Math.min(
+        ...to.map((t) => changeWord(t, trace.concat(t))).filter((t) => t)
+      ) + 1
     );
   };
-  return dfs(begin, [begin]);
+
+  return changeWord(begin, [begin]);
 }
