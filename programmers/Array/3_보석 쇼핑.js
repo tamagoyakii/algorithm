8de72e031bd27938f02187612b7d85fbf5b9f2a7
tamagoyakii <rlawlgus4063@naver.com>
2023-11-20 17:37:13 +0900

    [feata] 보석 쇼핑

--- programmers/Array/3_보석 쇼핑.js
+++ programmers/Array/3_보석 쇼핑.js
@@ -19,3 +19,29 @@ function solution(gems) {
   return [min + 1, max + 1];
 }
 // 효율성 테스트 15번 실패
+
+function solution(gems) {
+  const gemCount = new Map();
+  const types = [...new Set(gems)];
+  let [min, max] = [0, gems.length - 1];
+  let length = max - min;
+
+  for (let i = 0, j = 0; j < gems.length; j++) {
+    gemCount.set(gems[j], (gemCount.get(gems[j]) || 0) + 1);
+
+    while (gemCount.size === types.length) {
+      if (j - i < length) {
+        [min, max] = [i, j];
+        length = max - min;
+      }
+
+      gemCount.set(gems[i], gemCount.get(gems[i]) - 1);
+      if (gemCount.get(gems[i]) === 0) {
+        gemCount.delete(gems[i]);
+      }
+      i++;
+    }
+  }
+
+  return [min + 1, max + 1];
+}
