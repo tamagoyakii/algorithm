c0b6524385bbec8eaf7020c836eb29fbcfd34a41
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-14 22:36:16 +0900

    [Bronze I] Title: 팰린드롬수, Time: 124 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -1,9 +1,8 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
+input.splice(input.length - 2);
 input.forEach((num) => {
-  if (num === '0' || !num) return;
   num = '_' + num;
-
   for (let j = 1; j < num.length / 2; j++) {
     if (num.at(j) !== num.at(-j)) {
       console.log('no');
