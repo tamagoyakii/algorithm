fb6afed5fb640698c77f2d532b0ceceba55a1257
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-17 23:26:33 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -3,12 +3,9 @@ const N = Number(input);
 const cache = Array(N + 1).fill(0);
 
 for (let i = 2; i <= N; i++) {
-  const prev = [];
-
-  prev.push(cache[i - 1]);
-  i % 2 === 0 && prev.push(cache[i / 2]);
-  i % 3 === 0 && prev.push(cache[i / 3]);
-  cache[i] = Math.min(...prev) + 1;
+  cache[i] = cache[i - 1] + 1;
+  if (i % 2 === 0) cache[i] = Math.min(cache[i / 2] + 1, cache[i]);
+  if (i % 3 === 0) cache[i] = Math.min(cache[i / 3] + 1, cache[i]);
 }
 
 console.log(cache[N]);
