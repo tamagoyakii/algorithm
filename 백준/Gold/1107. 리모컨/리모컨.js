0f5b109915c43686ac4aaf38e9287ec2d6d80801
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-17 16:05:46 +0900

    [Gold V] Title: 리모컨, Time: 332 ms, Memory: 18972 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.js
+++ 백준/Gold/1107. 리모컨/리모컨.js
@@ -1,18 +1,12 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const unavailableButtons = new Set(input[2].split(' '));
+const unavailableButtons = new RegExp(input[2].split(' ').join('|') || 'n');
 const targetChannel = Number(input[0]);
 let min = Math.abs(targetChannel - 100);
 
 for (let n = 0; n < 1000000; n++) {
   const str = n.toString();
-  let i;
-
-  for (i = 0; i < str.length; i++) {
-    if (unavailableButtons.has(str[i])) break;
-  }
-  if (i === str.length) {
-    min = Math.min(min, str.length + Math.abs(targetChannel - n));
-  }
+  if (unavailableButtons.test(str)) continue;
+  min = Math.min(min, str.length + Math.abs(targetChannel - n));
 }
 
 console.log(min);
