14ff622f70cb5b143b542b5574e73f5985d92a47
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-18 15:27:53 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 324 ms, Memory: 50376 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -1,10 +1,10 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 const [N, M] = input[0].split(' ').map(Number);
-const problems = input.slice(N + 1, N + M + 1);
 const pokedex = new Map();
 
 for (let i = 1; i <= N; i++) pokedex.set(input[i], i);
 
+const problems = input.slice(N + 1, N + M + 1);
 const solution = problems
   .map((p) => {
     const i = Number(p);
