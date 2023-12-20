462ec772357c1e0bc7094061442c78ec4df85c2c
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-20 15:55:46 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 42256 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -3,19 +3,14 @@ const N = Number(input[0]);
 const meetings = input
   .slice(1, N + 1)
   .map((el) => el.split(' ').map(Number))
-  .sort(([s1, e1], [s2, e2]) =>
-    e1 < e2 ? -1 : e1 > e2 ? 1 : s1 < s2 ? -1 : 1
-  );
+  .sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
 
 let cnt = 0;
 
-meetings.reduce(
-  (acc, cur) => {
-    if (cur[0] < acc[1]) return acc;
-    cnt++;
-    return cur;
-  },
-  [0, 0]
-);
+meetings.reduce((acc, cur) => {
+  if (cur[0] < acc) return acc;
+  cnt++;
+  return cur[1];
+}, 0);
 
 console.log(cnt);
