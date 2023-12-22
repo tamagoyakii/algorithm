c183e8f1194a4c55c792c3e8acbe60f7cdb9b674
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-23 03:28:14 +0900

    [Silver II] Title: 색종이 만들기, Time: 196 ms, Memory: 13092 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -9,43 +9,36 @@ console.log(solution());
 function solution() {
   const N = Number(input[0]);
   const paper = input.slice(1).map((row) => row.split(' ').map(Number));
+  const colorCnt = [0, 0];
 
-  const solidColor = (pos, len) => {
-    const color = paper[pos[1]][pos[0]];
+  const solidColor = (x, y, n) => {
+    const color = paper[y][x];
 
-    for (let i = pos[1]; i < pos[1] + len; i++) {
-      for (let j = pos[0]; j < pos[0] + len; j++) {
+    for (let i = y; i < y + n; i++) {
+      for (let j = x; j < x + n; j++) {
         if (paper[i][j] !== color) return -1;
       }
     }
     return color;
   };
 
-  const cutPaper = (pos, len) => {
-    const color = solidColor(pos, len);
-    const colorCnt = [0, 0];
+  const cutPaper = (x, y, n) => {
+    const color = solidColor(x, y, n);
 
     if (color !== -1) {
-      colorCnt[color] = 1;
-      return colorCnt;
+      colorCnt[color]++;
+      return;
     }
 
-    const halfLen = len / 2;
-    const splits = [
+    n /= 2;
+    [
       [0, 0],
-      [0, halfLen],
-      [halfLen, 0],
-      [halfLen, halfLen],
-    ];
-
-    return splits.reduce(
-      (acc, [x, y]) => {
-        const [white, blue] = cutPaper([pos[0] + x, pos[1] + y], halfLen);
-        return [acc[0] + white, acc[1] + blue];
-      },
-      [0, 0]
-    );
+      [0, n],
+      [n, 0],
+      [n, n],
+    ].forEach(([sx, sy]) => cutPaper(x + sx, y + sy, n));
   };
 
-  return cutPaper([0, 0], N).join('\n');
+  cutPaper(0, 0, N);
+  return colorCnt.join('\n');
 }
