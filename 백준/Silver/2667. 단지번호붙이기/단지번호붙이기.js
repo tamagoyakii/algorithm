0d63b090712baa39f44ab6ee46edcabb1ed1e4c1
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-23 14:46:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 176 ms, Memory: 11220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -9,7 +9,6 @@ console.log(solution());
 function solution() {
   const N = Number(input[0]);
   const map = input.slice(1).map((row) => row.split('').map(Number));
-  const visited = Array.from({ length: N }, () => Array(N).fill(false));
   const dir = [
     [1, 0],
     [0, 1],
@@ -20,13 +19,12 @@ function solution() {
   const findComplex = (x, y) => {
     let cnt = 1;
 
-    visited[y][x] = true;
+    map[y][x] = 0;
     dir.forEach(([dx, dy]) => {
       const [nx, ny] = [x + dx, y + dy];
 
       if (nx < 0 || nx >= N || ny < 0 || ny >= N) return;
-      if (map[ny][nx] === 0 || visited[ny][nx]) return;
-      cnt += findComplex(nx, ny);
+      if (map[ny][nx]) cnt += findComplex(nx, ny);
     });
     return cnt;
   };
@@ -35,10 +33,10 @@ function solution() {
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      map[i][j] === 1 && !visited[i][j] && complexes.push(findComplex(j, i));
+      map[i][j] && complexes.push(findComplex(j, i));
     }
   }
 
-  complexes.sort((a, b) => a - b).unshift(complexes.length);
-  return complexes.join('\n');
+  complexes.sort((a, b) => a - b);
+  return `${complexes.length}\n${complexes.join('\n')}`;
 }
