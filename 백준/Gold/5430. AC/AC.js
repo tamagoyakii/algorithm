f794a75ffc402211943b47292e346c2bd358c72f
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-26 22:52:58 +0900

    [Gold V] Title: AC, Time: 240 ms, Memory: 25448 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -11,12 +11,11 @@ function solution() {
 
   for (let i = 1; i < input.length; i += 3) {
     const lan = input[i];
-    let n = Number(input[i + 1]);
     let arr = JSON.parse(input[i + 2]);
     let isForward = true;
     let isError = false;
     let startIndex = 0;
-    let endIndex = n;
+    let endIndex = Number(input[i + 1]);
 
     for (let i = 0; i < lan.length; i++) {
       if (lan[i] === 'R') isForward = !isForward;
