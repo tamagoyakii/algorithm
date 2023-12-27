2c8b910d0a8a96acc67a05f1b2fb1122f4b7129d
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-27 17:14:34 +0900

    [Silver I] Title: IOIOI, Time: 184 ms, Memory: 12024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -8,12 +8,22 @@ console.log(solution());
 
 function solution() {
   const N = Number(input[0]);
+  const M = Number(input[1]);
   const S = input[2];
-  const P = 'IO'.repeat(N) + 'I';
   let cnt = 0;
 
-  for (let i = S.indexOf(P); i !== -1; cnt++) {
-    i = S.indexOf(P, i + 1);
+  for (let i = 0; i < M; i++) {
+    if (S[i] === 'I') {
+      let j = i + 1;
+      let n = 0;
+
+      for (; j <= M; j += 2, n++) {
+        if (S[j] !== 'O' || S[j + 1] !== 'I') break;
+      }
+      if (n >= N) cnt += n - N + 1;
+      i = j - 1;
+    }
   }
+
   return cnt;
 }
