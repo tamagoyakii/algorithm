0a6b12587fb170f36de29a73221a6965411760bf
Jihyun Kim <rlawlgus2588@gmail.com>
2023-12-28 00:14:12 +0900

    [Gold V] Title: 토마토, Time: 964 ms, Memory: 123208 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.js
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -17,25 +17,20 @@ function solution() {
     [-1, 0, 0],
   ];
   let queue = [];
+  let day = 0;
 
   const boxes = Array.from({ length: H }, (_, i) =>
     Array.from({ length: N }, (__, j) =>
       input[i * N + j + 1].split(' ').map((el, k) => {
         const n = Number(el);
+
         if (n === 1) queue.push([k, j, i]);
         return n;
       })
     )
   );
 
-  const inBox = (x, y, z) => {
-    if (x < 0 || y < 0 || z < 0 || x >= M || y >= N || z >= H) return false;
-    return true;
-  };
-
   if (queue.length === M * N * H) return 0;
-
-  let day = 0;
 
   while (queue.length) {
     const newQueue = [];
@@ -44,7 +39,15 @@ function solution() {
       dir.forEach((d) => {
         const [dx, dy, dz] = [x + d[0], y + d[1], z + d[2]];
 
-        if (inBox(dx, dy, dz) && boxes[dz][dy][dx] === 0) {
+        if (
+          dx >= 0 &&
+          dy >= 0 &&
+          dz >= 0 &&
+          dx < M &&
+          dy < N &&
+          dz < H &&
+          boxes[dz][dy][dx] === 0
+        ) {
           newQueue.push([dx, dy, dz]);
           boxes[dz][dy][dx] = 1;
         }
