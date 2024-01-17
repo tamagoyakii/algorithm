cc7196c501ad15d49ce746fe1066c15ce9fef529
Jihyun Kim <rlawlgus2588@gmail.com>
2024-01-17 16:16:33 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 120 ms, Memory: 9740 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
@@ -1,27 +1,25 @@
+function solution(input) {
+  const res = [];
+  const it = input[Symbol.iterator]();
+  let N = Number(it.next().value);
+
+  while (N--) {
+    const att = {};
+    let M = Number(it.next().value);
+
+    while (M--) {
+      const key = it.next().value.split(' ')[1];
+      att[key] = (att[key] ?? 0) + 1;
+    }
+    res.push(Object.values(att).reduce((acc, cur) => acc * (cur + 1), 1) - 1);
+  }
+  return res.join('\n');
+}
+
 const input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .split('\n');
 
-console.log(solution());
-
-function solution() {
-  const N = Number(input[0]);
-  const res = [];
-
-  for (let i = 0, j = 1; i < N; i++) {
-    const M = Number(input[j++]);
-    const attributes = {};
-
-    for (let k = 0; k < M; k++) {
-      const key = input[j + k].split(' ')[1];
-      attributes[key] = (attributes[key] ?? 0) + 1;
-    }
-    res.push(
-      Object.values(attributes).reduce((acc, cur) => acc * (cur + 1), 1) - 1
-    );
-    j += M;
-  }
-  return res.join('\n');
-}
+console.log(solution(input));
