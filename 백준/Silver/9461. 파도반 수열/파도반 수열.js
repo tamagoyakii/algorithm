8bc8318f16561f3dbaca55c81e02ee17f7986296
Jihyun Kim <rlawlgus2588@gmail.com>
2024-01-18 18:07:40 +0900

    [Silver III] Title: 파도반 수열, Time: 116 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -1,21 +1,12 @@
 function solution(input) {
-  const res = [];
-  const it = input[Symbol.iterator]();
+  const [_, ...arr] = input.map(Number);
+  const max = Math.max(...arr);
   const dp = [0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9];
-  let T = Number(it.next().value);
 
-  while (T--) {
-    const N = Number(it.next().value);
-    if (N < dp.length) {
-      res.push(dp[N]);
-      continue;
-    }
-    for (let i = dp.length; i <= N; i++) {
-      dp[i] = dp[i - 1] + dp[i - 5];
-    }
-    res.push(dp[N]);
+  for (let i = dp.length; i <= max; i++) {
+    dp[i] = dp[i - 1] + dp[i - 5];
   }
-  return res.join('\n');
+  return arr.map((N) => dp[N]).join('\n');
 }
 
 const input = require('fs')
