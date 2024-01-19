f914f2c0ebe0ae81dea25ec1cae6ec1933f9ccb8
Jihyun Kim <rlawlgus2588@gmail.com>
2024-01-19 15:59:18 +0900

    [Gold V] Title: 적록색약, Time: 184 ms, Memory: 10612 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -1,47 +1,29 @@
 function solution(input) {
   const N = Number(input[0]);
-  const maps = {
-    A: input.slice(1),
-    B: input.slice(1).map((row) => row.replace(/G/g, 'R')),
-  };
-  const visits = {
-    A: Array.from({ length: N }, () => Array(N).fill(false)),
-    B: Array.from({ length: N }, () => Array(N).fill(false)),
-  };
-  let A = 0;
-  let B = 0;
+  const original = input.slice(1).map((r) => r.split(''));
+  const colorblind = input.slice(1).map((r) => r.replace(/G/g, 'R').split(''));
+  const ans = [0, 0];
 
-  const dir = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
-
-  const dfs = (type, x, y) => {
-    visits[type][y][x] = true;
-    dir.forEach(([dx, dy]) => {
-      const [nx, ny] = [x + dx, y + dy];
-      if (nx < 0 || ny < 0 || nx >= N || ny >= N) return;
-      if (!visits[type][ny][nx] && maps[type][y][x] === maps[type][ny][nx]) {
-        dfs(type, nx, ny);
-      }
-    });
+  const dfs = (painting, color, x, y) => {
+    if (x < 0 || y < 0 || x >= N || y >= N || painting[y][x] !== color) return;
+    painting[y][x] = -1;
+    dfs(painting, color, x + 1, y);
+    dfs(painting, color, x - 1, y);
+    dfs(painting, color, x, y + 1);
+    dfs(painting, color, x, y - 1);
+    return true;
   };
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (!visits.A[i][j]) {
-        A++;
-        dfs('A', j, i);
-      }
-      if (!visits.B[i][j]) {
-        B++;
-        dfs('B', j, i);
-      }
+      if (original[i][j] !== -1 && dfs(original, original[i][j], j, i))
+        ans[0]++;
+      if (colorblind[i][j] !== -1 && dfs(colorblind, colorblind[i][j], j, i))
+        ans[1]++;
     }
   }
-  return `${A} ${B}`;
+
+  return ans.join(' ');
 }
 
 const input = require('fs')
