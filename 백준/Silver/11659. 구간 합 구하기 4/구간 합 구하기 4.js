104c6aa26bc03a386234fdbc63a1302667ad85ad
Jihyun Kim <rlawlgus2588@gmail.com>
2024-01-24 22:39:11 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 352 ms, Memory: 47392 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -2,14 +2,14 @@ function solution(input) {
   const [N, M] = input.shift().split(' ').map(Number);
   const numbers = input.shift().split(' ').map(Number);
   const sum = Array(N + 1).fill(0);
-  const res = [];
 
   numbers.forEach((num, i) => (sum[i + 1] = sum[i] + num));
-  for (let i = 0; i < M; i++) {
-    const [start, end] = input[i].split(' ').map(Number);
-    res.push(sum[end] - sum[start - 1]);
-  }
-  return res.join('\n');
+  return input
+    .map((el) => {
+      const [start, end] = el.split(' ').map(Number);
+      return sum[end] - sum[start - 1];
+    })
+    .join('\n');
 }
 
 const input = require('fs')
