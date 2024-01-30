ac39acdeb3a73c1bce84254e62e29093ba09be11
Jihyun Kim <rlawlgus2588@gmail.com>
2024-01-30 14:41:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 200 ms, Memory: 9656 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,17 @@
+function solution(input) {
+  const N = Number(input[0]);
+  const dp = [0, 1, 3];
+
+  for (let i = 3; i <= N; i++) {
+    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10_007;
+  }
+  return dp[N];
+}
+
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+console.log(solution(input));
