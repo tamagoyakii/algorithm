d5dd3cf484238182cf76a34d2b4f4f0f7599d326
tamagoyakii <rlawlgus4063@naver.com>
2024-02-13 20:32:15 +0900

    [delete]

--- programmers/Array/3_최고의 집합.js
+++
@@ -1,9 +0,0 @@
-function solution(n, s) {
-  const average = Math.floor(s / n);
-  const lack = s - average * n;
-
-  if (average === 0) return [-1];
-  return new Array(n - lack)
-    .fill(average)
-    .concat(new Array(lack).fill(average + 1));
-}
