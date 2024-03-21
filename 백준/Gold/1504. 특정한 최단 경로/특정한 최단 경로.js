19362b602c819ef108d278536ae6e122080f5723
Jihyun Kim <rlawlgus2588@gmail.com>
2024-03-21 18:32:00 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 768 ms, Memory: 79016 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.js
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.js
@@ -1,10 +1,10 @@
 function solution(input) {
-  const it = input[Symbol.iterator]();
-  let [N, E] = it.next().value.split(' ').map(Number);
+  const [N, E] = input[0].split(' ').map(Number);
+  const [v1, v2] = input[E + 1].split(' ').map(Number);
   const route = Array.from({ length: N + 1 }, () => []);
 
-  while (E--) {
-    const [start, end, cost] = it.next().value.split(' ').map(Number);
+  for (let i = 1; i <= E; i++) {
+    const [start, end, cost] = input[i].split(' ').map(Number);
     route[start].push([end, cost]);
     route[end].push([start, cost]);
   }
@@ -15,20 +15,19 @@ function solution(input) {
     dist[start] = 0;
 
     while (queue.length) {
-      const [pos, cost] = queue.shift();
-      if (dist[pos] < cost) continue;
+      const [cur, cost] = queue.shift();
+      if (dist[cur] < cost) continue;
 
-      route[pos].forEach(([des, desCost]) => {
+      route[cur].forEach(([des, desCost]) => {
         if (dist[des] > cost + desCost) {
           dist[des] = cost + desCost;
-          queue.push([des, cost + desCost]);
+          queue.push([des, dist[des]]);
         }
       });
     }
     return dist;
   };
 
-  const [v1, v2] = it.next().value.split(' ').map(Number);
   const dijkstra1 = dijkstra(1);
   const dijkstraN = dijkstra(N);
   const dijkstraV = dijkstra(v1);
