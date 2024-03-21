b38c474b7dd5afbb7d9e5c4cd461a051b7cc0159
Jihyun Kim <rlawlgus2588@gmail.com>
2024-03-21 14:25:37 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.76 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,6 @@
 function solution(A,B){
-    var answer = 0;
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    for (let i = 0; i < A.length; i++) answer += A[i] * B[i];
-    return answer;
+    
+    return A.reduce((acc, cur, i) => acc + cur * B[i], 0)
 }
