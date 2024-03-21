660bff81903f36101fc91c349d941e8136cac61b
Jihyun Kim <rlawlgus2588@gmail.com>
2024-03-21 16:33:43 +0900

    [Gold III] Title: 파티, Time: 216 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.js
+++ 백준/Gold/1238. 파티/파티.js
@@ -1,39 +1,41 @@
 function solution(input) {
   const it = input[Symbol.iterator]();
   const [N, M, X] = it.next().value.split(' ').map(Number);
-  const graph = Array.from({ length: N + 1 }, () => []);
+  const forwardGraph = Array.from({ length: N + 1 }, () => []);
+  const backwardGraph = Array.from({ length: N + 1 }, () => []);
 
   for (let i = 0; i < M; i++) {
     const [start, end, time] = it.next().value.split(' ').map(Number);
-    graph[start].push([end, time]);
+    forwardGraph[start].push([end, time]);
+    backwardGraph[end].push([start, time]);
   }
 
-  const dijkstra = (start) => {
+  const dijkstra = (graph) => {
     const times = Array(N + 1).fill(Infinity);
-    const queue = [start];
-    times[start] = 0;
+    const queue = [[X, 0]];
+    times[X] = 0;
 
     while (queue.length) {
-      const current = queue.shift();
+      const [start, time] = queue.shift();
 
-      graph[current].forEach(([next, time]) => {
-        if (times[next] > times[current] + time) {
-          times[next] = times[current] + time;
-          queue.push(next);
+      graph[start].forEach(([d, t]) => {
+        if (times[d] > time + t) {
+          times[d] = time + t;
+          queue.push([d, times[d]]);
         }
       });
     }
     return times;
   };
 
-  const dijkstraX = dijkstra(X);
+  const dijkstraForward = dijkstra(forwardGraph);
+  const dijkstraBackward = dijkstra(backwardGraph);
+
   let max = 0;
 
   for (let i = 1; i <= N; i++) {
-    const dijkstraI = dijkstra(i);
-    const min = dijkstraI[X] + dijkstraX[i];
-
-    if (min > max) max = min;
+    const sum = dijkstraForward[i] + dijkstraBackward[i];
+    if (sum > max) max = sum;
   }
   return max;
 }
