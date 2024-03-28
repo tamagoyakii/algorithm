57fb4ac6a130751b6b6c166ba55dade36a64dfb0
Jihyun Kim <rlawlgus2588@gmail.com>
2024-03-28 22:17:07 +0900

    [level 2] Title: 할인 행사, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,14 +1,19 @@
 function solution(want, number, discount) {
-    const obj = {};
-    
-    want.forEach((el, i) => obj[el] = number[i]);
-    return discount.reduce((acc, cur, i) => {
-        const tmp = {...obj};
-        for (let j = i; j < i + 10; j++) {
-            if (!tmp[discount[j]]) continue;
-            if (tmp[discount[j]] === 1) delete tmp[discount[j]];
-            else tmp[discount[j]] -= 1;
-        }
-        return Object.keys(tmp).length ? acc : acc + 1;
-    }, 0);
+  const wantMap = new Map(want.map((item, index) => [item, number[index]]));
+  let ans = 0;
+
+  for (let start = 0; start <= discount.length - 10; start++) {
+    const tmp = new Map(wantMap);
+    for (let i = start; i < start + 10; i++) {
+      const item = discount[i];
+      if (tmp.has(item)) {
+        const remaining = tmp.get(item) - 1;
+        if (remaining === 0) tmp.delete(item);
+        else tmp.set(item, remaining);
+      }
+    }
+    if (tmp.size === 0) ans++;
+  }
+
+  return ans;
 }
